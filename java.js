7bbef4316591967be1bad7e90a1c3c06a1286826
Blake <blaketannehill@gmail.com>
2023-12-15 21:23:11 -0800

    creating function for cmptr choice

--- java.js
+++ java.js
@@ -1,3 +1,10 @@
+//Make computer choose random choice: Rock, Papper, Scissors
+
 function getComputerChoice (choice) {
-    
+    if (choice.length === 0)
+    throw new error ("Choice should not be empty")
+    const choicearray = ["Rock, Paper, Scissors"];
+    const computerchoice = math.floor(mathrandom().choice.length);
 }
+
+
