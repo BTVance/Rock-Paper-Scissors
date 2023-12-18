7796c1a354b387e770119474f5399ca2bdf3307c
Blake <blaketannehill@gmail.com>
2023-12-18 12:30:37 -0800

    Wrote function for user input, returning undefined. Also started writing function for Rock Paper sciss round

--- java.js
+++ java.js
@@ -4,5 +4,25 @@ function getComputerChoice () {
     const choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random(choices)*choices.length)];
 }
+const computerSelection = getComputerChoice
 
-let x = getComputerChoice()
+let x = getComputerChoice();
+
+function playerChoice() {
+    prompt("Please Choose Rock, Paper, or Scissors")
+}
+const playerSelection = playerChoice
+let z = playerSelection()
+
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Rock")
+        return "It's a Tie! Play again";
+    else if (playerSelection === "Paper" && computerSelection ==="Rock")
+        return "You win the round!";
+    else if (playerSelection === "Rock" && computerSelection ==="Scissors")
+        return "You win the round!";
+    else if (playerSelection === "Scissors" && computerSelection ==="Paper")
+        return "You win the round!";
+}
